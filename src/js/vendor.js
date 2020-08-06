599a51aee559920a332325409ba8c7d0b8f55555
Zino-chan <webdevzino@gmail.com>
2020-08-06 14:33:34 +0100

    Removed fontawsome

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,11 +1,5 @@
 import '@glidejs/glide/dist/css/glide.core.min.css';
 
-//Font-awesome
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-
 //Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
